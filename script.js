905a475d27f0a793b37982f20f4e332d7d0e56bf
VedantHandore <vedanthandore15@gmail.com>
2024-12-04 00:44:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-document.getElementById('yesButton').addEventListener('click', function() {
-    document.getElementById('responseMessage').classList.remove('hidden');
-    document.getElementById('yesButton').disabled = true;
-    document.getElementById('noButton').disabled = true;
-});
-
-document.getElementById('noButton').addEventListener('mouseover', function() {
-    const button = document.getElementById('noButton');
-    button.style.position = 'absolute';
-    button.style.top = Math.random() * (window.innerHeight - button.clientHeight) + 'px';
-    button.style.left = Math.random() * (window.innerWidth - button.clientWidth) + 'px';
-});
+function showMessage() {
+    const message = document.getElementById("hiddenMessage");
+    message.classList.toggle("hidden");
+  }
+  
